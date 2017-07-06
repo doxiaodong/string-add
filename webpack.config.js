5bd44de2cf99756aad645df0ab7a458f3aab6eaf
duxiaodong <duxiaodong@darlin.me>
2017-07-06 20:50:26 +0800

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack')
 const path = require('path')
-const BabiliPlugin = require("babili-webpack-plugin")
+const BabiliPlugin = require('babili-webpack-plugin')
 
 module.exports = {
   devtool: 'source-map',
